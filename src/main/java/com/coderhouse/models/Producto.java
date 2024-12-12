a88983d3f14f5ccf9c5d1120693a55b81bfeca18
Hector Berrutti <82546028+TitoBerru@users.noreply.github.com>
2024-12-12 19:24:46 -0300

    add mapping to db

--- src/main/java/com/coderhouse/models/Producto.java
+++ src/main/java/com/coderhouse/models/Producto.java
@@ -2,29 +2,221 @@ package com.coderhouse.models;
 
 import java.math.BigDecimal;
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos") 
 public class Producto {
-	
+	@Id // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // para volverlo autoincremental
 	private Long id;
-	private String nombre;
-	private String descripcion;
-	private String categoria;
-	private String marca;
-	private String proveedor;
-	private String imgUrl;
+	
+	@Column(nullable = false, length = 100)
+	private String nombreProducto;
+	
+	@Column(length = 500)
+	private String descripcionProducto;
+	
+	@Column(nullable = false, length = 50)
+	private String categoriaProducto;
+	
+	@Column(length = 50)
+	private String marcaProducto;
+	 
+	 @Column(length = 100)
+	private String proveedorProducto;
+	 
+	@Column(length = 200)
+	private String imgUrlProducto;
+	
+	 @Column(nullable = false)
+	private int cantidadStockProducto;
+	 
+	 @Column(nullable = false)
+	private BigDecimal precioCompraProducto;
+	 
+	@Column(nullable = false)
+	private BigDecimal precioVentaProducto;
 	
 	
-	private int cantidadStock;
-	private BigDecimal precioCompra;
-	private BigDecimal precioVenta;
-	private BigDecimal descuento;
-	private String moneda;
+	private BigDecimal descuentoProducto;
+	
+	 @Column(length = 3)
+	private String monedaProducto;
 	 
 	
-	private Date fechaAlta;
-	private Date fechaModificacion;
+	private Date fechaAltaProducto;
+	private Date fechaModificacionProducto;
 	
-	private boolean estadoActivo;
+	@Column(nullable = false)
+	private boolean estadoActivoProducto;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(
+			name = "producto_cliente", 
+			joinColumns = @JoinColumn(name= "producto_id"), 
+			inverseJoinColumns = @JoinColumn(name = "cliente_id"))
+	
+	private List<Cliente> clientes = new ArrayList<>();
+
+	public Producto() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombreProducto() {
+		return nombreProducto;
+	}
+
+	public void setNombreProducto(String nombreProducto) {
+		this.nombreProducto = nombreProducto;
+	}
+
+	public String getDescripcionProducto() {
+		return descripcionProducto;
+	}
+
+	public void setDescripcionProducto(String descripcionProducto) {
+		this.descripcionProducto = descripcionProducto;
+	}
+
+	public String getCategoriaProducto() {
+		return categoriaProducto;
+	}
+
+	public void setCategoriaProducto(String categoriaProducto) {
+		this.categoriaProducto = categoriaProducto;
+	}
+
+	public String getMarcaProducto() {
+		return marcaProducto;
+	}
+
+	public void setMarcaProducto(String marcaProducto) {
+		this.marcaProducto = marcaProducto;
+	}
+
+	public String getProveedorProducto() {
+		return proveedorProducto;
+	}
+
+	public void setProveedorProducto(String proveedorProducto) {
+		this.proveedorProducto = proveedorProducto;
+	}
+
+	public String getImgUrlProducto() {
+		return imgUrlProducto;
+	}
+
+	public void setImgUrlProducto(String imgUrlProducto) {
+		this.imgUrlProducto = imgUrlProducto;
+	}
+
+	public int getCantidadStockProducto() {
+		return cantidadStockProducto;
+	}
+
+	public void setCantidadStockProducto(int cantidadStockProducto) {
+		this.cantidadStockProducto = cantidadStockProducto;
+	}
+
+	public BigDecimal getPrecioCompraProducto() {
+		return precioCompraProducto;
+	}
+
+	public void setPrecioCompraProducto(BigDecimal precioCompraProducto) {
+		this.precioCompraProducto = precioCompraProducto;
+	}
+
+	public BigDecimal getPrecioVentaProducto() {
+		return precioVentaProducto;
+	}
+
+	public void setPrecioVentaProducto(BigDecimal precioVentaProducto) {
+		this.precioVentaProducto = precioVentaProducto;
+	}
+
+	public BigDecimal getDescuentoProducto() {
+		return descuentoProducto;
+	}
+
+	public void setDescuentoProducto(BigDecimal descuentoProducto) {
+		this.descuentoProducto = descuentoProducto;
+	}
+
+	public String getMonedaProducto() {
+		return monedaProducto;
+	}
+
+	public void setMonedaProducto(String monedaProducto) {
+		this.monedaProducto = monedaProducto;
+	}
+
+	public Date getFechaAltaProducto() {
+		return fechaAltaProducto;
+	}
+
+	public void setFechaAltaProducto(Date fechaAltaProducto) {
+		this.fechaAltaProducto = fechaAltaProducto;
+	}
+
+	public Date getFechaModificacionProducto() {
+		return fechaModificacionProducto;
+	}
+
+	public void setFechaModificacionProducto(Date fechaModificacionProducto) {
+		this.fechaModificacionProducto = fechaModificacionProducto;
+	}
+
+	public boolean isEstadoActivoProducto() {
+		return estadoActivoProducto;
+	}
+
+	public void setEstadoActivoProducto(boolean estadoActivoProducto) {
+		this.estadoActivoProducto = estadoActivoProducto;
+	}
+
+	public List<Cliente> getClientes() {
+		return clientes;
+	}
+
+	public void setClientes(List<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombreProducto=" + nombreProducto + ", descripcionProducto="
+				+ descripcionProducto + ", categoriaProducto=" + categoriaProducto + ", marcaProducto=" + marcaProducto
+				+ ", proveedorProducto=" + proveedorProducto + ", imgUrlProducto=" + imgUrlProducto
+				+ ", cantidadStockProducto=" + cantidadStockProducto + ", precioCompraProducto=" + precioCompraProducto
+				+ ", precioVentaProducto=" + precioVentaProducto + ", descuentoProducto=" + descuentoProducto
+				+ ", monedaProducto=" + monedaProducto + ", fechaAltaProducto=" + fechaAltaProducto
+				+ ", fechaModificacionProducto=" + fechaModificacionProducto + ", estadoActivoProducto="
+				+ estadoActivoProducto + ", clientes=" + clientes + "]";
+	}
+
+	
+	
 	
 	
 }
